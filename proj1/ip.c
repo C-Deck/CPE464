b7d8cd73c12dac28a36afddc0e5717eed90a7c95
Clint Desktop <cdecker18@gmail.com>
2020-04-21 23:04:15 -0700

    Checksum TCP

--- proj1/ip.c
+++ proj1/ip.c
@@ -93,13 +93,14 @@ int pseudoHeader(uint8_t *pseudoHeader, struct ipHeader *header) {
    pseudoHeader[9] = header->PROTOCOL;
    //*((uint16_t *) (pseudoHeader + 10)) = (uint8_t *) &tcp_size;
    //memcpy(pseudoHeader + 10, &tcp_size, 2);
-   pseudoHeader[10] = (uint8_t) *((uint8_t *) ptr);
-   pseudoHeader[11] = *(ptr + 1);
-   //pseudoHeader[10] = ((tcp_size & 0xff00) >> 8);
+   //pseudoHeader[10] = (uint8_t) *((uint8_t *) ptr);
+   //pseudoHeader[11] = *(ptr + 1);
+   pseudoHeader[10] = ((tcp_size & 0xff00) >> 8);
 
-   //pseudoHeader[11] = tcp_size & 0x00ff;
+   pseudoHeader[11] = tcp_size & 0x00ff;
    printf("\nTCP SIZE %x04\n", tcp_size);
    printf("\n Value wanted -- %x ----- Got: %x  --- and -- %x\n", ((tcp_size & 0xff00) >> 8), *ptr, *(ptr + 1));
+   printf("Final value wanted: %x\n", *((uint16_t *)(pseudoHeader + 10)));
 
    return tcp_size;
 }
