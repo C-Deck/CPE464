016fe7ad24348fd6cc76234c89e4dbff48455d42
Clint Desktop <cdecker18@gmail.com>
2020-04-21 19:17:04 -0700

    TCP v2

--- proj1/tcp.c
+++ proj1/tcp.c
@@ -13,11 +13,11 @@ void getTCP(const uint8_t *packetData, int tcp_size, uint8_t *psuedoHeader) {
 	int byteAdjustment = 0;
 
 	/* SOURCE PORT*/
-	header->SOURCE_PORT = ntohs(*((u_int16_t *) packetData));
+	header->SOURCE_PORT = ntohs(*((uint16_t *) packetData));
 	byteAdjustment = byteAdjustment + 2;
 
 	/* DESTINATION PORT*/
-    header->DEST_PORT = ntohs(((u_int16_t *) packetData + byteAdjustment));
+    header->DEST_PORT = ntohs(*((uint16_t *) packetData + byteAdjustment));
 	byteAdjustment = byteAdjustment + 2;
 
 	/* Sequence Number */
@@ -57,5 +57,5 @@ void printTCP(struct tcpHeader *header, uint16_t checksum) {
 	printf("\t\tRST Flag: %s\n", (header->FLAGS & RST_MASK) ? "Yes" : "No");
 	printf("\t\tFIN Flag: %s\n", (header->FLAGS & FIN_MASK) ? "Yes" : "No");
 	printf("\t\tWindow Size: %d\n", header->WINDOW);
-	printf("\t\tChecksum: %s (%x)\n", checksum ? "Correct" : "Incorrect", header->CHECKSUM);
+	printf("\t\tChecksum: %s (0x%x)\n", checksum ? "Correct" : "Incorrect", header->CHECKSUM);
 }
