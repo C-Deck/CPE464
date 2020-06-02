e27f40b8304e6a4349b63766b3fb621290d43954
Clint Desktop <cdecker18@gmail.com>
2020-06-01 20:37:56 -0700

    Debugging

--- proj3/util.c
+++ proj3/util.c
@@ -178,8 +178,8 @@ int32_t recvCall(uint8_t *dataBuffer, uint32_t len, int32_t socket, UDPConnectio
 
     memcpy(&checksum, &(aPDU[4]), 2);
 
-    if ((checksumResult = in_cksum((unsigned short *) aPDU, dataLen)) != checksum) {
-		printf("Bad checksum: %d - Expected: %d\n", checksumResult, checksum);
+    if ((checksumResult = in_cksum((unsigned short *) aPDU, dataLen)) != 0) {
+		printf("Bad checksum: %d\n", checksumResult);
         return RECV_ERROR;
     }
 
